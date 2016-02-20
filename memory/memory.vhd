6e27784d7deada690e41defa557f5e1ab1631e69
Stefan Cao <caostefan@gmail.com>
2016-02-20 11:58:47 -0800

    testing memory

--- memory/memory.vhd
+++ memory/memory.vhd
@@ -113,14 +113,15 @@ end process;
 memory_process: process (ref_clk, WE, IorD, addr, WD)
 
 	variable D_mem_var : D_memory;
-	variable i_mem: ramtype;
+	--variable i_mem: ramtype;
 	
 
 		if(ref_clk'event AND ref_clk='1') then 
-			if(IorD='0') then
-				RD<= i_mem(to_integer(addr)) & i_mem(to_integer(addr) + 1) 
-						& i_mem(to_integer(addr) +2) & i_mem(to_integer(addr) + 3);
-			else 	
+
+			--if(IorD='0') then
+				--RD<= i_mem(to_integer(addr)) & i_mem(to_integer(addr) + 1) 
+						--& i_mem(to_integer(addr) +2) & i_mem(to_integer(addr) + 3);
+			--else 	
 				if(WE='1') then
 					D_mem_var(to_integer(unsigned(addr))) := WD(31 downto 24);
 					D_mem_var(to_integer(unsigned(addr))+1) := WD(23 downto 16);
@@ -131,7 +132,7 @@ memory_process: process (ref_clk, WE, IorD, addr, WD)
 					RD <= D_mem_var(to_integer(unsigned(addr))) &  D_mem_var(to_integer(unsigned(addr))+1)
 						& D_mem_var(to_integer(unsigned(addr))+2) & D_mem_var(to_integer(unsigned(addr))+3);
 				end if;
-			end if;
+			--end if;
 		end if;
 
 	end process;
