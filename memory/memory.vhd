9cdd0a1cb37c65c673d9b4495bfcd2a82774feb1
Stefan Cao <caostefan@gmail.com>
2016-02-20 12:38:51 -0800

    testing old rom

--- memory/memory.vhd
+++ memory/memory.vhd
@@ -42,108 +42,36 @@ END memory;
 architecture behavior of memory is
 
 subtype byte is std_logic_vector(7 DOWNTO 0);
+type I_memory is array (0 to 63) of byte;
 type D_memory is array (0 to (2**11)-1) of byte; --size: 8 x 2048
-type ramtype is array (2**16 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
+
+constant I_mem: I_memory := ("00100000","00000010","00000000","00000101",
+			"00100000","00000011","00000000","00001100",
+			"00100000","01100111","11111111","11110111",
+			"00000000","11100010","00100000","00100101",
+			"00000000","10100100","00101000","00100000",
+			"00010000","10100111","00000000","00001010",
+			"00000000","01100100","00100000","00101010",
+			"00010000","10000000","00000000","00000001",
+			"00100000","00000101","00000000","00000000",
+			"00000000","11100010","00100000","00101010",
+			"00000000","10000101","00111000","00100000",
+			"00000000","11100010","00111000","00100010",
+			"10101100","01100111","00000000","01000100",
+			"10001100","00000010","00000000","01010000",
+			"00001000","00000000","00000000","00010001",
+			"00100000","00000010","00000000","00000001");--,
+			--"10101100","00000010","00000000","01010100");
+
 
 begin
-
-
-
-READ_FILE: process
-file mem_file: TEXT;
-variable L: line;
-variable ch: character;
-variable i, index, result: integer;
-
-
-
-variable mem: ramtype;
-begin
-for i in 0 to 63 loop 
-mem(i) := (others => '0');
-end loop;
-
-index := 0;
-FILE_OPEN (mem_file, "imem.h", READ_MODE);
-while not endfile(mem_file) loop
-readline(mem_file, L);
-result := 0;
-
-	for i in 1 to 8 loop
-	read (L, ch);
-		if '0' <= ch and ch <= '9' then
-			result := character'pos(ch) - character'pos('0');
-		elsif 'a' <= ch and ch <= 'f' then
-			result := character'pos(ch) - character'pos('a') + 10;
-		else report "Format error on line" & integer'
-			image(index) severity error;
-		end if;
-		if (i = 1 OR i = 2) then
-		mem(index)(11-i*4 DOWNTO 8-i*4) := to_std_logic_vector(result,4);
-		elsif (i = 3 OR i = 4) then
-		mem(index + 1)(19-i*4 DOWNTO 16-i*4) := to_std_logic_vector(result,4);
-		elsif (i = 5 OR i = 6) then
-		mem(index + 2)(27-i*4 DOWNTO 24-i*4) := to_std_logic_vector(result,4);
-		elsif (i = 7 OR i = 8) then
-		mem(index + 3)(35-i*4 DOWNTO 32-i*4) := to_std_logic_vector(result,4);
-		end if;
-	end loop; -- end for loop
-index := index + 4;
-end loop; -- end while
-end process;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-memory_process: process (ref_clk, WE, IorD, addr, WD)
-	
-
-	variable D_mem_var : D_memory;
-	variable mem: ramtype;
-	begin	
-
-		if(ref_clk'event AND ref_clk='1') then 
-			if(IorD='0') then
-				RD<= mem(to_integer(addr)) & mem(to_integer(addr) + 1) 
-						& mem(to_integer(addr) +2) & mem(to_integer(addr) + 3);
-			else 	
-				if(WE='1') then
-					D_mem_var(to_integer(unsigned(addr))) := WD(31 downto 24);
-					D_mem_var(to_integer(unsigned(addr))+1) := WD(23 downto 16);
-					D_mem_var(to_integer(unsigned(addr))+2) := WD(15 downto 8);
-					D_mem_var(to_integer(unsigned(addr))+3) := WD(7 downto 0);
-
-				else
-					RD <= D_mem_var(to_integer(unsigned(addr))) &  D_mem_var(to_integer(unsigned(addr))+1)
-						& D_mem_var(to_integer(unsigned(addr))+2) & D_mem_var(to_integer(unsigned(addr))+3);
-				end if;
-			end if;
-		end if;
-
-	end process;
-end behavior;
-
-
-
-
-
-
-
+	if(ref_clk'event AND ref_clk='1') then 
+		RD<= I_mem(to_integer(addr(7 DOWNTO 0))) & 
+				I_mem(to_integer(addr(7 DOWNTO 0)) + 1) & 
+				I_mem(to_integer(addr(7 DOWNTO 0)) +2) & 
+				I_mem(to_integer(addr(7 DOWNTO 0)) + 3);
+	end if;
+end;
 
 
 
